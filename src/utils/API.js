910b295c1f3f6cc6b0c37180b17ea5b20bad22d7
shukri74 <shukri.mohamed@mail.com>
2023-03-18 23:11:43 +0000

    added quote api function in the api file

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,45 @@
-import axios from "axios";
+//import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
-export default {
-  getBooks: function() {
-    return axios.get("https://openlibrary.org/works/OL45804W/editions.json");
-  },
+//export default {
+  //getBooks: function() {
+    //return axios.get("https://openlibrary.org/works/OL45804W/editions.json");
+  //},
 //   getDogsOfBreed: function(breed) {
 //     return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
 //   },
 //   getBaseBreedsList: function() {
 //     return axios.get("https://dog.ceo/api/breeds/list");
 //   }
-};
+//};
+
+import axios from 'axios';
+import { useState } from 'react';
+import 'bulma/css/bulma.min.css';
+function App() {
+  
+    const [quote, setQuote] = useState ("")
+      const getQuote = () => {
+        axios.get("https://api.quotable.io/random").then(Response =>{
+          setQuote(Response.data.content)
+
+        }).catch (Error =>{
+          
+
+        })
+
+        
+
+
+      }
+      return (
+      <div className="app">
+        {quote && <p>{quote}</p>}
+        <button onClick={getQuote}>quote</button>
+
+    </div>
+      );
+}
+
+export default App;
