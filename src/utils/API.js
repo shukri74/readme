cf269dfff299c5877c46ee0c5c6b66c25fedcfff
Haroon <mharoonbutt13@gmail.com>
2023-03-18 16:05:24 +0000

    book gallery component added

---
+++ src/utils/API.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
+export default {
+  getBooks: function() {
+    return axios.get("https://openlibrary.org/works/OL45804W/editions.json");
+  },
+//   getDogsOfBreed: function(breed) {
+//     return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
+//   },
+//   getBaseBreedsList: function() {
+//     return axios.get("https://dog.ceo/api/breeds/list");
+//   }
+};
