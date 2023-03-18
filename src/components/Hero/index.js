c8020f03c28175e39005259f7eb8b0c97a42f664
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-03-18 18:05:28 +0000

    added some css styling to the hero image and aligned text

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,8 +3,27 @@ import "./style.css";
 
 function Hero(props) {
   return (
-    <div className="hero text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
-      {props.children}
+    <div className="hero is-transparent is-fullheight-with-navbar text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
+      <div className="hero-body">
+        <div className="container">
+        <div className="columns is-centered">
+            <div className="column is-two-thirds">
+          <h1 className="title">
+            There have been over 130,000,000 books published since the invention of printing
+          </h1>
+          <h2 className="subtitle">
+            Find the right book for you:
+          </h2>
+          {/* {props.children} */}
+          <div className="field">
+            <div className="control is-large">
+              <input className="input is-primary is-rounded" type="text" placeholder="Search for a book" style={{width: '450px'}}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
     </div>
   );
 }
