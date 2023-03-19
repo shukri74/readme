5fb9a6db9f7ff8676808b37c24c2ef6859823fe6
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-03-19 23:33:32 +0000

    added animation to hero text to make more dynamic

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,32 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
 function Hero(props) {
+  const [showFirstText, setShowFirstText] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowFirstText((prev) => !prev);
+    }, 9000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="hero is-transparent is-fullheight-with-navbar text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
       <div className="hero-body">
         <div className="container">
-        <div className="columns is-centered">
+          <div className="columns is-centered">
             <div className="column is-two-thirds">
-          <h1 className="title">
-            There have been over 130,000,000 books published since the invention of printing
-          </h1>
-          <h2 className="subtitle">
-            Find the right book for you:
-          </h2>
-          {/* {props.children} */}
-          <div className="field">
-            <div className="control is-large">
-              <input className="input is-primary is-rounded" type="text" placeholder="Search for a book" style={{width: '450px'}}/>
+              <div className={showFirstText ? "typed-text" : "typed-text hidden"}>
+                <h1 className="title">
+                  There have been over 130,000,000 books published since the invention of printing
+                </h1>
+                <h2 className="subtitle">
+                  Find one for you:
+                </h2>
+              </div>
+              <div className={!showFirstText ? "typed-text" : "typed-text hidden"}>
+                <h1 className="title">
+                  Explore millions of books and find your next adventure:
+                </h1>
+              </div>
+              <div className="field">
+                <div className="control is-large">
+                  <input className="input is-primary is-rounded" type="text" placeholder="Search for a book" style={{width: '450px'}}/>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-    </div>
-  )};
+  );
+}
 
-
- 
 export default Hero;
