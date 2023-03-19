4e8a2f8350925b4edeedeea8d71c97ec61d453d2
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-03-19 13:36:55 +0000

    changed the API so that it generates quotes without having to click a button

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'bulma/css/bulma.min.css';
 
 function Quote() {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
-  
-  const getQuote = () => {
+
+  const fetchQuote = () => {
     axios.get("https://api.quotable.io/random").then(response => {
       setQuote(response.data.content);
       setAuthor(response.data.author);
@@ -14,7 +14,15 @@ function Quote() {
       console.log(error);
     });
   }
-  
+
+  useEffect(() => {
+    fetchQuote();
+    const interval = setInterval(() => {
+      fetchQuote();
+    }, 6000); // replace with the interval time you prefer in milliseconds
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className="hero is-warning is-medium">
       <div className="hero-body">
@@ -22,7 +30,6 @@ function Quote() {
           <div className="content has-text-centered">
             {quote && <p style={{fontSize: "1.5rem", fontWeight: "bold"}}>{quote}</p>}
             {author && <p style={{fontSize: "1rem",fontWeight: "bold"}} >by {author}</p>}
-            <button className="button is-warning" onClick={getQuote}>Inspirational quotes</button>
           </div>
         </div>
       </div>
