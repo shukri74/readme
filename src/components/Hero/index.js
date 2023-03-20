7b22c783abef5f92a65bd1b8fecd7e4d606a24c7
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-03-20 21:59:37 +0000

    installin packages

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,54 +1,38 @@
-import React, { useState, useEffect } from "react";
-import "./style.css";
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import AboutUsHero from "./abt";
 import QuoteHero from "./quoting";
 
 function Hero(props) {
-  const [showAboutUs, setShowAboutUs] = useState(false);
-  const [showQuote, setShowQuote] = useState(false);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setShowAboutUs((prev) => !prev);
-    }, 9000);
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setShowQuote((prev) => !prev);
-    }, 18000);
-    return () => clearInterval(interval);
-  }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
   return (
-    <>
-      {showAboutUs && <AboutUsHero />}
-      {showQuote && <QuoteHero />}
-      {!showAboutUs && !showQuote && (
-        <div
-          className="hero is-transparent is-fullheight-with-navbar text-center"
-          style={{ backgroundImage: `url(${props.backgroundImage})` }}
-        >
+    <Slider {...settings}>
+      <div>
+        <div className="hero is-transparent is-fullheight-with-navbar text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
           <div className="hero-body">
             <div className="container">
               <div className="columns is-centered">
                 <div className="column is-two-thirds">
-                  <div className="typed-text">
-                    <h1 className="title">
-                      There have been over 130,000,000 books published since
-                      the invention of printing
-                    </h1>
-                    <h2 className="subtitle">Find one for you:</h2>
-                  </div>
+                  <h1 className="title">
+                    There have been over 130,000,000 books published since the invention of printing
+                  </h1>
+                  <h2 className="subtitle">
+                    Find the right book for you:
+                  </h2>
+                  {/* {props.children} */}
                   <div className="field">
                     <div className="control is-large">
-                      <input
-                        className="input is-primary is-rounded"
-                        type="text"
-                        placeholder="Search for a book"
-                        style={{ width: "450px" }}
-                      />
+                      <input className="input is-primary is-rounded" type="text" placeholder="Search for a book" style={{width: '450px'}}/>
                     </div>
                   </div>
                 </div>
@@ -56,8 +40,14 @@ function Hero(props) {
             </div>
           </div>
         </div>
-      )}
-    </>
+      </div>
+      <div>
+        <AboutUsHero />
+      </div>
+      <div>
+        <QuoteHero />
+      </div>
+    </Slider>
   );
 }
 
