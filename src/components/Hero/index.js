376377b693f8a48b02631aef29699e479af7826d
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-03-20 19:50:07 +0000

    trying out sliding the hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -27,7 +27,7 @@ function Hero(props) {
               </div>
               <div className={!showFirstText ? "typed-text" : "typed-text hidden"}>
                 <h1 className="title">
-                  Explore millions of books and find your next adventure:
+                  Explore millions of books and find your next adventure
                 </h1>
               </div>
               <div className="field">
