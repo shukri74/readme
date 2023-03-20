e91d00418b02fec3d31410b89751f81cc6060ecc
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-03-20 14:33:37 +0000

    made minor changes

--- src/components/QuoteSection/index.js
+++ src/components/QuoteSection/index.js
@@ -34,8 +34,8 @@ function Quotes() {
     return (
       <div className="quote-container">
         <div className="quote-content has-text-centered">
-          {quote && <p style={{fontSize: "1.5rem", fontWeight: "bold"}}>{quote}</p>}
-          {author && <p style={{fontSize: "1rem",fontWeight: "bold"}} >by {author}</p>}
+          {quote && <p style={{fontSize: "1.5rem", fontWeight: "bold", fontFamily: "arial"}}>{quote}</p>}
+          {author && <p style={{fontSize: "1rem",}} >by {author}</p>}
         </div>
       </div>
     );
--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import QuoteSection from "../components/QuoteSection";
-
+import "../pages/about.css";
 import API from "../utils/API";
 import BookGallery from "../components/BookGallery";
 
