11551152760e8f98c848fa86d1ef4943758bcb73
shukri74 <shukri.mohamed@mail.com>
2023-03-20 23:45:45 +0000

    installed fontawesome,added footer and changed color of navbar to green

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "../Footer/style.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Footer() {
   return (
-    <footer className="footer">
-      <span>Copyright 2023.All Rights Reserved.</span>
+    <footer className="footer" style={{ backgroundColor: 'green' }}>
+      <div className="content has-text-centered">
+        <p>
+          <strong>Developed by:</strong>
+          <a href="https://github.com/shukri74"><span className="icon"><i className="fab fa-github"></i></span>Shukri</a>,
+          <a href="https://github.com/JaneDoe"><span className="icon"><i className="fab fa-github"></i></span>Haroon</a>,
+          <a href="https://github.com/MaxDoe"><span className="icon"><i className="fab fa-github"></i></span>Jason</a>, and
+          <a href="https://github.com/SaraDoe"><span className="icon"><i className="fab fa-github"></i></span>Eniola</a>.
+        </p>
+      </div>
     </footer>
   );
 }
