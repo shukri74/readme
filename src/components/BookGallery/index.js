5ed6b1a85ad94579a775d342a1de1e0b2ca74d4d
Haroon <mharoonbutt13@gmail.com>
2023-03-23 12:19:15 +0000

    try to deploy on netlify

--- src/components/BookGallery/index.js
+++ src/components/BookGallery/index.js
@@ -25,6 +25,8 @@ const BookGallery = () => {
           throw new Error(res.data.message);
         }
         setBookState(res.data.entries);
+
+      
        
         
       })
@@ -33,22 +35,24 @@ const BookGallery = () => {
   
 // newlink = https://covers.openlibrary.org/b/isbn/9780385533225-S.jpg
 // oldlink = "https://covers.openlibrary.org/b/olid/"+ imageUrl(book.key) +".jpg"
-  const bookList = bookState.filter((book, idx) => idx < 10).map((book) =>
-    <div className='column is-half' key={book.key}>
-      <div className="card-image">
-          <figure className="image is-1by1">
-              <img src={"https://covers.openlibrary.org/b/olid/"+ imageUrl(book.key) +".jpg"} alt={book.title}/>
-          </figure>
-          <div className="card-content">
-              <p className="title is-4"><strong>Book Title: </strong>{book.title}</p>
-              <p className="subtitle is-6"><strong>Publisher: </strong>{book.publishers}</p>
-            <div className="content">
-              <time><strong>Published in </strong>{book.publish_date}</time>
+
+      const bookList = bookState.filter((book, idx) => idx < 10).map((book) =>
+      <div className='column is-half' key={book.key}>
+        <div className="card-image">
+            <figure className="image is-1by1">
+                <img src={"https://covers.openlibrary.org/b/olid/"+ imageUrl(book.key) +".jpg"} alt={book.title}/>
+            </figure>
+            <div className="card-content">
+                <p className="title is-4"><strong>Book Title: </strong>{book.title}</p>
+                <p className="subtitle is-6"><strong>Publisher: </strong>{book.publishers}</p>
+              <div className="content">
+                <time><strong>Published in </strong>{book.publish_date}</time>
+              </div>
             </div>
-          </div>
+        </div>
       </div>
-    </div>
-  );
+      );
+ 
 
     return (
       <div className='backg-cont' style={{ background: 'linear-gradient(to bottom, #43ff8b, #26c4b9)' }}>
