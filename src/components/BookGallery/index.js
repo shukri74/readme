8d0a4cc0f7ab3cf686b7ace79c45eff4f7228d6f
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-03-23 01:48:00 +0000

    trying to debug

--- src/components/BookGallery/index.js
+++ src/components/BookGallery/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
-import Book from "../Book";
+// import Book from "../Book";
 
 const BookGallery = () => {
   const [bookState, setBookState] = useState([]);
